73ae819873b7bc0a9dd5b14d6346ffeb818fdbcc
chungmcl <chungmcl@hotmail.com>
2021-06-11 13:22:29 -0700

    Initial Commit

---
+++ KanjiSensei/KanjiSenseiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanjiSenseiApp.swift
+//  KanjiSensei
+//
+//  Created by Micheal Chung on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct KanjiSenseiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
