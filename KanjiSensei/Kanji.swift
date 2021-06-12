40b3ad9ae401ddba070b962c9f7ee9bef635f79d
chungmcl <chungmcl@hotmail.com>
2021-06-12 00:36:40 -0700

    first commit

---
+++ KanjiSensei/Kanji.swift
@@ -0,0 +1,8 @@
+//
+//  Kanji.swift
+//  KanjiSensei
+//
+//  Created by Micheal Chung on 6/11/21.
+//
+
+import Foundation
---
+++ KanjiSensei/Words.swift
@@ -0,0 +1,8 @@
+//
+//  Words.swift
+//  KanjiSensei
+//
+//  Created by Micheal Chung on 6/11/21.
+//
+
+import Foundation
