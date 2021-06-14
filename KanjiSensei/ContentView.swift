1cdedb8c61be01f34582e05e6459b61666fb6561
chungmcl <chungmcl@hotmail.com>
2021-06-14 16:16:18 -0700

    Added URLImage

--- KanjiSensei/ContentView.swift
+++ KanjiSensei/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import URLImage
 
 struct ContentView: View {
     @State private var wordToAdd: String = ""
@@ -16,38 +17,10 @@ struct ContentView: View {
         Spacer()
         VStack {
             if (self.word != nil) {
-                
-                RemoteImage(url: self.word!.tokens[self.selectedTokenIdx].kanji[0].spectrumStrokeOrderDiagramUrl)
-                           .aspectRatio(contentMode: .fit)
-                           .frame(width: 200)
-                
-                HStack {
-                    ForEach(word!.tokens, id: \.id) { token in
-                        Button(action: {
-                            updateSelectedIdx(token: token)
-                        }) {
-                            VStack {
-                                // Furigana of word token
-                                Text(token.pronunciation)
-                                    .font(
-                                        (token.kanji.count > 0) ?
-                                            Font.system(size: 10, weight: .heavy, design: .default) :
-                                            Font.system(size: 10, weight: .ultraLight, design: .default)
-                                )
-                                // Word token
-                                Text(token.string)
-                                    .font(
-                                        (token.kanji.count > 0) ?
-                                        Font.system(size: 30, weight: .heavy, design: .default) :
-                                        Font.system(size: 30, weight: .ultraLight, design: .default)
-                                )
-                            }
-                        }
-                        .buttonStyle(PlainButtonStyle())
-                        // Disabled if does not contain a kanji
-                        .disabled(token.kanji.count <= 0);
-                    }
+                URLImage(self.word!.tokens[self.selectedTokenIdx].kanji[0].spectrumStrokeOrderDiagramUrl!) { image in
+                    image.resizable().aspectRatio(contentMode: .fit)
                 }
+                TokensView(word: self.$word, selectedTokenIdx: self.$selectedTokenIdx)
             }
             
             TextField("Word to add...", text: self.$wordToAdd, onCommit: {
@@ -57,16 +30,47 @@ struct ContentView: View {
         }
     }
     
-    private func updateSelectedIdx(token: Token) {
-        self.selectedTokenIdx = self.word!.tokens.firstIndex(of: token)!
-    }
-    
     private func getWord() {
         do {
             self.word = try Word(string: self.wordToAdd)
         }
         catch {
             print("Fail")
+        }
+    }
+}
+
+struct TokensView: View {
+    @Binding var word: Word?
+    @Binding var selectedTokenIdx: Int
+    
+    var body: some View {
+        HStack {
+            ForEach(word!.tokens, id: \.id) { token in
+                Button(action: {
+                    self.selectedTokenIdx = self.word!.tokens.firstIndex(of: token)!
+                }) {
+                    VStack {
+                        // Furigana of word token
+                        Text(token.pronunciation)
+                            .font(
+                                (token.kanji.count > 0) ?
+                                    Font.system(size: 10, weight: .heavy, design: .default) :
+                                    Font.system(size: 10, weight: .ultraLight, design: .default)
+                        )
+                        // Word token
+                        Text(token.string)
+                            .font(
+                                (token.kanji.count > 0) ?
+                                Font.system(size: 30, weight: .heavy, design: .default) :
+                                Font.system(size: 30, weight: .ultraLight, design: .default)
+                        )
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+                // Disabled if does not contain a kanji
+                .disabled(token.kanji.count <= 0);
+            }
         }
     }
 }
