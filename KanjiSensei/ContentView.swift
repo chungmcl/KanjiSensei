44702b694befd1f0f67b798ded62d33757c57ec0
chungmcl <chungmcl@hotmail.com>
2021-07-14 11:33:20 -0700

    Working on UI

--- KanjiSensei/ContentView.swift
+++ KanjiSensei/ContentView.swift
@@ -11,12 +11,45 @@ import URLImage
 struct ContentView: View {
     
     var body: some View {
+        // Navigation takes two "arguments" in { } -- first is sidebar, second is content view. Otherwise, just
+        // declare first "argument" and use a NavigationLink for the content view
         NavigationView {
-            NavigationLink(destination: WordView()) {
-                Text("Test")
-            }
-            .buttonStyle(PlainButtonStyle())
+            
+                List {
+                    HStack {
+                        Text("My Sets")
+                            .font(Font.system(size: 20, weight: .bold, design: .default))
+                            .foregroundColor(.primary)
+                        Spacer()
+                        Button(action: {
+                            self.addNewSet()
+                        }) {
+                            Text("New Set")
+                                .padding(5)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 7)
+                                        .stroke(Color.gray, lineWidth: 1)
+                                )
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                    
+                    NavigationLink(destination: WordView()) {
+                        Text("N2 List")
+                    }
+                }
+                .listStyle(SidebarListStyle())
+            
         }
         .navigationViewStyle(DoubleColumnNavigationViewStyle())
+        //.toolbar {
+        //    Button("Button") {
+        //
+        //    }
+        //}
+    }
+    
+    private func addNewSet() {
+    
     }
 }
